b26cd31fa596f3cb56607fefd8b0bf6446430472
errorzzz <longreddogz@gmail.com>
2013-07-07 18:24:33 +0400

    edited web.js

--- web.js
+++ web.js
@@ -1,12 +1,6 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var fs = require('fs');
+fs.readFile('index.html', function(err, buf) {
+  console.log(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
